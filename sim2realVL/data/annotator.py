9e7651963b6b88348a43da347171646ca37ad8ec
gtziafas <pullmypill@hotmail.com>
2021-11-19 21:32:23 +0200

    to anydesk

--- sim2realVL/data/annotator.py
+++ sim2realVL/data/annotator.py
@@ -16,6 +16,9 @@ def annotate_dataset(dataset: SimScenesDataset, write_file: str):
         graph = parser(scene)
         queries, truths = [], []
         categories = set([o.category for o in graph.nodes])
+        category_count = Counter([o.category for o in graph.nodes])
+        ambiguous = [i for i, o in enumerate(graph.nodes) if category_count[o.category] > 1]     
+        
         for cat in categories:
             # multi-label annotations for ambigious queries
             idces = [i for i, o in enumerate(graph.nodes) if o.category == cat]
@@ -25,10 +28,68 @@ def annotate_dataset(dataset: SimScenesDataset, write_file: str):
                 continue
 
             # additional annotations for ambigious objects
-            for idx in idces:
-                obj = graph.nodes[idx]
+            for i in idces:
+                obj = graph.nodes[i]
+
+                # add one color annot per ambiguous object
                 queries.append(' '.join([obj.color, cat]))
-                truths.append([idx])
+                truths.append([i])
+
+                # and up to three spatial annots 
+                # behind/in-front, next to, left/right
+                x_vector = graph.edges[i, :, 0]
+                y_vector = graph.edges[i, :, 1]
+                d_vector = graph.edges[i, :, 2]
+                SOURCE = obj.category          
+
+                proposed_queries = []
+                # search for behind / in front of :
+                if 1 in y_vector or -1 in y_vector:
+                    target_obj_idx = choice(np.where(np.bitwise_or(y_vector==1, y_vector==-1))[0])
+                    SPT = "behind" if y_vector[target_obj_idx] == -1 else "in front of"
+                    target_obj = graph.nodes[target_obj_idx]
+                    if target_obj_idx in ambiguous:
+                        TARGET = ' '.join([target_obj.color, target_obj.category])
+                    
+                    else:
+                        TARGET = target_obj.category
+
+                    proposed_queries.append(' '.join([SOURCE, SPT, TARGET]))
+                
+                # search for next to (dist < threshold):
+                if d_vector.min() <= 100:
+                    target_obj_idx = np.argmin(d_vector)
+                    SPT = "next to"
+                    target_obj = graph.nodes[target_obj_idx]
+                    if target_obj_idx in ambiguous:
+                        TARGET = ' '.join([target_obj.color, target_obj.category])  
+                    else:
+                        TARGET = target_obj.category
+
+                    proposed_queries.append(' '.join([SOURCE, SPT, TARGET]))
+
+
+                # add a left / right
+                target_obj_idx = choice(np.where(np.bitwise_or(x_vector==1, x_vector==-1))[0])
+                SPT = "left from" if x_vector[target_obj_idx] == -1 else "right from"
+                target_obj = graph.nodes[target_obj_idx]
+                if target_obj_idx in ambiguous:
+                    TARGET = ' '.join([target_obj.color, target_obj.category])
+                
+                else:
+                    TARGET = target_obj.category
+
+                proposed_queries.append(' '.join([SOURCE, SPT, TARGET]))
+
+                # check if query already given and append truths (2nd order ambiguity)
+                for q in proposed_queries:
+                    if q in queries:
+                        query_idx = queries.index(q)
+                        truths[query_idx] += [i]
+                    else:
+                        queries.append(proposed_query)
+                        truths.append([i])
+
 
         # spatial
         category_count = Counter([o.category for o in graph.nodes])
